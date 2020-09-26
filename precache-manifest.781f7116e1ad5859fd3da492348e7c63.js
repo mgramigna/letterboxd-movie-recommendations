82fd910acc352fc004bb0899df404aaf59ab34e6
Matt Gramigna <matthewgramigna@gmail.com>
2020-09-25 21:26:44 -0400

    Updates

---
+++ precache-manifest.781f7116e1ad5859fd3da492348e7c63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4817b9eb58a9a7743f87c3e434f203",
+    "url": "/letterboxd-movie-recommendations/index.html"
+  },
+  {
+    "revision": "83194fda0c6c1134841c",
+    "url": "/letterboxd-movie-recommendations/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b778e3a67295b3042c5c",
+    "url": "/letterboxd-movie-recommendations/static/js/2.be246fe0.chunk.js"
+  },
+  {
+    "revision": "57fb53616680b7493fba4a0fc85989d2",
+    "url": "/letterboxd-movie-recommendations/static/js/2.be246fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83194fda0c6c1134841c",
+    "url": "/letterboxd-movie-recommendations/static/js/main.ecb1ecd4.chunk.js"
+  },
+  {
+    "revision": "f6211ab0d028477bf81e",
+    "url": "/letterboxd-movie-recommendations/static/js/runtime-main.2c402839.js"
+  }
+]);
