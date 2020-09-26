07adc96c0159a24ae9a4f8bc9c2d5002250cc9a7
Matt Gramigna <matthewgramigna@gmail.com>
2020-09-26 13:52:35 -0400

    Updates

--- precache-manifest.cfa075e8556263e477bd8580d75d4159.js
+++ precache-manifest.cfa075e8556263e477bd8580d75d4159.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "68908aa32b3ff83c57e335535aec8bea",
+    "revision": "ec61bcb21d86e64f498a2f761919d962",
     "url": "/letterboxd-movie-recommendations/index.html"
   },
   {
-    "revision": "2d9922416d1e14cf6a90",
+    "revision": "fd8a7f6aee5f6de67e0f",
     "url": "/letterboxd-movie-recommendations/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "db3c1eabc12a62a0eeff",
-    "url": "/letterboxd-movie-recommendations/static/js/2.87977b4f.chunk.js"
+    "revision": "48263ead3003831f20bc",
+    "url": "/letterboxd-movie-recommendations/static/js/2.b0d5a6ce.chunk.js"
   },
   {
     "revision": "57fb53616680b7493fba4a0fc85989d2",
-    "url": "/letterboxd-movie-recommendations/static/js/2.87977b4f.chunk.js.LICENSE.txt"
+    "url": "/letterboxd-movie-recommendations/static/js/2.b0d5a6ce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d9922416d1e14cf6a90",
-    "url": "/letterboxd-movie-recommendations/static/js/main.0f76f2a8.chunk.js"
+    "revision": "fd8a7f6aee5f6de67e0f",
+    "url": "/letterboxd-movie-recommendations/static/js/main.1c7a47ff.chunk.js"
   },
   {
     "revision": "f6211ab0d028477bf81e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/letterboxd-movie-recommendations/precache-manifest.5bdb8bfa71a3fae3f5eb46f0dfed3833.js"
+  "/letterboxd-movie-recommendations/precache-manifest.cfa075e8556263e477bd8580d75d4159.js"
 );
 
 self.addEventListener('message', (event) => {
